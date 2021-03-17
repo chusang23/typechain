d23c22cf262a04702262ae395575ca09826fe211
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-03-17 17:46:09 +0900

    blockchain

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lynn = new Human("Lynn", 18, "female");
-const person = {
-    name: "nicolas",
-    gender: "male",
-    age: 22
-};
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
-console.log(sayHi(lynn));
+const genesisBlock = new Block(0, "20202020202", "", "hello", 123456);
+let blockChain = [genesisBlock];
+console.log(blockChain);
 //# sourceMappingURL=index.js.map

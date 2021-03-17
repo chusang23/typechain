82b9205439e0fbcef6d8a76198b3375cecca054f
chusang23 <45816771+chusang23@users.noreply.github.com>
2021-03-17 17:11:42 +0900

    ts2

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const lynn = new Human("Lynn", 18, "female");
 const person = {
     name: "nicolas",
     gender: "male",
@@ -8,5 +16,5 @@ const person = {
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 //# sourceMappingURL=index.js.map
